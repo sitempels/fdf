37d5c3d3f257e898279b39e8d77d9b39d01babac
Simon Tempels <stempels@shi-r12-p1.s19.be>
2025-03-31 16:06:38 +0200

    start

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: stempels <stempels@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 13:05:23 by stempels          #+#    #+#             */
-/*   Updated: 2025/03/27 17:49:25 by stempels         ###   ########.fr       */
+/*   Updated: 2025/03/31 14:37:31 by stempels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,18 @@ typedef struct	s_data
 	int	line_length;
 	int	endian;
 }		t_data;
+
+typedef struct	s_point
+{
+	int			x;
+	t_point		xp;
+	t_point		xn;
+	int			y;
+	t_point		yp;
+	t_point		yn;
+	int			z;
+	unsigned int	color;
+}		t_point;
 /**/
 /*---------------------------FONCT--------------------------------------------*/
 /**/
--- src/main_fdf.c
+++ src/main_fdf.c
@@ -6,7 +6,7 @@
 /*   By: stempels <stempels@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 12:51:48 by stempels          #+#    #+#             */
-/*   Updated: 2025/03/27 18:41:47 by stempels         ###   ########.fr       */
+/*   Updated: 2025/03/31 15:27:10 by stempels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ int	main(int argc, char **argv)
 	int	i;
 	int	j;
 	void	*mlx_cnx;
+	t_point	*map;
 
 	i = ft_strlen(argv[argc - 1]);
 	j = 4;
 	while (--j <= 0)
 		if (argv[argc - 1][i + j] != EXT_FDF[j])
 			return (-1);
+	map = parse_map(argv[argc - 1]);
+	if (!map)
+		return (-1);
 	mlx_cnx = mlx_init();
 	fdf(mlx_cnx, argv[argc - 1]);
 	mlx_loop(mlx_cnx);
@@ -42,30 +46,72 @@ void	fdf(void *mlx_cnx, char *argv)
 	mlx_win = mlx_new_window(mlx_cnx, WIDHT, HEIGHT, &argv[5]);
 	img.img = mlx_new_image(mlx_cnx, WIDHT, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length, &img.endian);
-	i = WIDHT / 3;
-	while (i < WIDHT * 2 / 3)
-	{
-		j = HEIGHT / 3;
-		while (j < HEIGHT * 2 /3)
-		{
-			my_mlx_pxput(&img, i, j, 0x00FF000F);
-			j++;
-			i++;
-		}
-	}
-	i = WIDHT / 3;
-	while (i < WIDHT * 2 / 3)
-	{
-		j = HEIGHT * 2 / 3;
-		while (j > HEIGHT * 1 /3)
-		{
-			my_mlx_pxput(&img, i, j, 0x000000FF);
-			j--;
-			i++;
-		}
-	}
+
 	mlx_put_image_to_window(mlx_cnx, mlx_win, img.img, 0, 0);
 	return ;
+}
+
+t_point	*parse_map(char *mapfile)
+{
+	int	x;
+	int	fd;
+	char	*line;
+	t_point	*map;
+
+	fd = open(mapfile, 'r');
+	if (fd < 0)
+		return (NULL);
+	line = get_next_line(fd);
+	if (!line)
+		return (NULL);
+	map = init_struct(line, 0);
+	x = 1;
+	while (!line)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			return (NULL);
+		map = init_struct(line, x);
+		if (!map)
+			return (NULL);
+		x++;
+	}
+	map = link_struct(map);
+	if (!map)
+		return (NULL);
+	close (fd);
+	return (map);
+}
+
+t_point	*init_struct(char *line, int x)
+{
+	int	i;
+	t_point	pt;
+
+	i = 0;
+	while (line[i])
+	{
+		pt = (t_point) malloc(sizeof(t_point) * 1);	
+	}
+}
+
+void	draw_line(t_data *data, t_point *pt1, t_point *pt2)
+{
+	int	i;
+	int	j;
+	int	ptx;
+	int	pty;
+
+	i = 1;
+	j = 1;
+	ptx = pt1->x;
+	pty = pt1->y;
+	while (ptx < pt2->x || pty < pt2->y)
+	{
+		my_mlx_pxput(data, ptx, pty, ptcolor);
+		ptx = ptx + (i++ / (pt2->x - pt1->x));
+		pty = pty + (j++ / (pt2->x - pt1->x));
+	}
 }
 
 void	my_mlx_pxput(t_data *data, int x, int y, int color)
